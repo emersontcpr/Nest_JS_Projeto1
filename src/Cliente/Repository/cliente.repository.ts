8b6d0d36096e498e1d1002a1f1bc196cc50c100b
emersontcpr <emersontcpr@gmail.com>
2025-05-02 23:56:25 -0300

    Repository
    Metodos Gets, Add e remove

--- src/Cliente/Repository/cliente.repository.ts
+++ src/Cliente/Repository/cliente.repository.ts
@@ -1,6 +1,31 @@
 import { Injectable } from "@nestjs/common";
+import { ClienteEntity } from "../entity/Cliente.Entity";
+import { throws } from "assert";
 
 @Injectable()
 export class ClienteRepository {
+    private clienteArray: ClienteEntity[] = [];
 
+    async add(cliente: ClienteEntity) {
+        this.clienteArray.push(cliente);
+    }
+    async GetAll() {
+        this.clienteArray;
+    }
+    async GetForId(id: number) {
+        return this.clienteArray.find(
+            (x) => x.id === id
+        )
+    }
+    async GetForGuid(guid: string) {
+        return this.clienteArray.find(
+            (x) => x.guid === guid
+        )
+    }
+
+    async remove(id: number) {
+        this.clienteArray = this.clienteArray.filter(
+            (x) => x.id !== id
+        )
+    }
 }
--- src/Vendador/Repository/vendedor.repository.ts
+++ src/Vendador/Repository/vendedor.repository.ts
@@ -1,6 +1,31 @@
 import { Injectable } from "@nestjs/common";
+import { VendedorEntity } from "../entity/vendedor.entity";
 
 @Injectable()
-export class VendedorRepository{
-    
+export class VendedorRepository {
+    private vendedorArray: VendedorEntity[] = [];
+
+    async add(vendedor: VendedorEntity) {
+        this.vendedorArray.push(vendedor);
+    }
+    async getAll() {
+        return this.vendedorArray;
+    }
+
+    async getForId(id: number) {
+        return this.vendedorArray.find(
+            (x) => x.id === id,
+        );
+    }
+    async getForGuid(guid: string) {
+        return this.vendedorArray.find(
+            (x) => x.guid === guid,
+        );
+    }
+
+    async remove(id: number) {
+        this.vendedorArray = this.vendedorArray.filter(
+            (x) => x.id !== id
+        );
+    }
 }
